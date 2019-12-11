2da5f34574229259aaf095e64547cb42d246bf89
Achref TRIKI <achref@wizy.io>
2019-12-11 12:52:58 +0100

    update signup request form

--- src/main/java/com/grokonez/jwtauthentication/message/request/SignUpForm.java
+++ src/main/java/com/grokonez/jwtauthentication/message/request/SignUpForm.java
@@ -1,8 +1,12 @@
 package com.grokonez.jwtauthentication.message.request;
 
+import java.util.Date;
 import java.util.Set;
 
 import javax.validation.constraints.*;
+
+import com.grokonez.jwtauthentication.model.user.ColorName;
+import com.grokonez.jwtauthentication.model.user.GenderName;
 
 public class SignUpForm {
     @NotBlank
@@ -23,6 +27,13 @@ public class SignUpForm {
     @NotBlank
     @Size(min = 6, max = 40)
     private String password;
+    
+    private Date birthdate;
+    private GenderName gender;
+    private String description;
+    private ColorName hairColor;
+    private ColorName eyesColor;
+    private boolean blocked;
 
     public String getName() {
         return name;
@@ -63,4 +74,52 @@ public class SignUpForm {
     public void setRole(Set<String> role) {
     	this.role = role;
     }
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public ColorName getHairColor() {
+		return hairColor;
+	}
+
+	public void setHairColor(ColorName hairColor) {
+		this.hairColor = hairColor;
+	}
+
+	public ColorName getEyesColor() {
+		return eyesColor;
+	}
+
+	public void setEyesColor(ColorName eyesColor) {
+		this.eyesColor = eyesColor;
+	}
+
+	public boolean isBlocked() {
+		return blocked;
+	}
+
+	public void setBlocked(boolean blocked) {
+		this.blocked = blocked;
+	}
+
+	public Date getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public GenderName getGender() {
+		return gender;
+	}
+
+	public void setGender(GenderName gender) {
+		this.gender = gender;
+	}
 }
