0f1c9db6340913824104d91de1c9a476ecb70e5f
nidhal.bougatf@esprit.tn <nidhal.bougatf@esprit.tn>
2019-12-13 14:23:17 +0100

    service pacage added

---
+++ src/main/java/com/grokonez/jwtauthentication/service/Impl/UserService.java
@@ -0,0 +1,21 @@
+package com.grokonez.jwtauthentication.service.Impl;
+
+import com.grokonez.jwtauthentication.model.user.User;
+import com.grokonez.jwtauthentication.repository.UserRepository;
+import com.grokonez.jwtauthentication.service.interfaces.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+import java.util.List;
+@Component
+public class UserService implements IUserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+}
---
+++ src/main/java/com/grokonez/jwtauthentication/service/interfaces/IUserService.java
@@ -0,0 +1,10 @@
+package com.grokonez.jwtauthentication.service.interfaces;
+
+import com.grokonez.jwtauthentication.model.user.User;
+
+import java.util.List;
+//
+public interface IUserService {
+    public List<User> findAll();
+
+}
