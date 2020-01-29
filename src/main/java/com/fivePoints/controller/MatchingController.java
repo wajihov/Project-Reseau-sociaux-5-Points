b598faab4d302d4aa9dd3a01f60a91fdf2d6ddc1
wajihov <wajihov@hotmail.fr>
2020-01-29 11:13:58 +0100

    Begin in matching

--- src/main/java/com/fivePoints/controller/MatchingController.java
+++ src/main/java/com/fivePoints/controller/MatchingController.java
@@ -1,6 +1,7 @@
 package com.fivePoints.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class MatchingController {
 	
-	
+	@PostMapping("/matching/{idFrom}/{idTo}")
+	public void matchingFriend(Long idTo, Long idFrom) {
+		
+	}
 
 }
